009a73e37a498bf00aea3fe04382c8c076a94452
Dominik Przywara <dominik.przywara@gmail.com>
2017-01-16 02:27:23 +0100

    Network partially working (players cannot see that have taken creature) Network menu done Creatures manager done Camera changed

--- LowPolyTerrain/Assets/Scripts/States/GameStates/GameStateInit.cs
+++ LowPolyTerrain/Assets/Scripts/States/GameStates/GameStateInit.cs
@@ -3,17 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Scripts;
 using Assets.Scripts.Terrains;
 using UnityEngine;
 using Assets.Scripts.GUI.Game;
+using UnityEngine.Networking;
 
 namespace Assets.Scripts.States.GameStates
 {
     class GameStateInit:State
     {
+        private NetworkManagerHUD hud;
+
         protected override IEnumerator Init()
         {
             TerrainController terrain = GameObject.FindObjectOfType<TerrainController>();
+            hud = GameObject.FindObjectOfType<NetworkManagerHUD>();
+            hud.showGUI = false;
 
             yield return terrain.GenerateTerrain(LoadingPanel.OnProgress);
             yield return base.Init();
@@ -22,6 +28,12 @@ namespace Assets.Scripts.States.GameStates
         protected override IEnumerator End()
         {
             LoadingPanel.Disable();
+
+            hud.showGUI = true;
+            var a = GameObject.FindObjectOfType<CustomNetworkManager>();
+            //a.StartHost();
+            
+
             return base.End();
         }
     }
