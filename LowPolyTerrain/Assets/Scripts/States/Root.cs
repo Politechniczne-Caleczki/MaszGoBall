f87d106676e9ed63cfb9e6e837887eeba03dcd5e
Michał Milczanowski <milczanowskimichal@gmail.com>
2017-03-14 15:25:22 +0100

    path 1.2

---
+++ LowPolyTerrain/Assets/Scripts/States/Root.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.States;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace States
+{
+    class Root: State
+    {
+        public Root()
+        {
+            AddState<MenuState>(this);
+            AddState<GameState>(this);
+        }
+
+        protected internal IEnumerator Enable()
+        {
+            yield return _Activate<MenuState>();
+        }
+
+        protected internal void _Update()
+        {
+            Update();
+        }
+    }
+}
